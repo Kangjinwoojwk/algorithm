6b3b0189e3f9a31e250689f084aaedd0bd1f8a7e
Kangjinwoojwk <kjw03230@naver.com>
2019-03-10 23:22:19 +0900

    algorithm/0310

--- swea/1767.py
+++ swea/1767.py
@@ -1,83 +1,45 @@
 import sys
 sys.stdin = open('1767.txt','r')
-def sol(n, line_lenth, connect):
+def sol(n, connect, line_lenth):
     global N, ans, max_connect, processor_cnt
-    if n == processor_cnt:
+    if processor_cnt == n:
         if max_connect < connect:
             max_connect = connect
             ans = line_lenth
         elif max_connect == connect and ans > line_lenth:
             ans = line_lenth
         return
-    for i in range(4):
-        if i == 0:
-            for j in range(1, N):
-                if processor[n][0] - j == -1:
-                    line_lenth += (j - 1)
-                    connect += 1
-                    for k in range(1, j):
-                        board[processor[n][0] - k][processor[n][1]] = 1
-                    sol(n + 1, line_lenth, connect)
-                    for k in range(1, j):
-                        board[processor[n][0] - k][processor[n][1]] = 0
-                    connect -= 1
-                    line_lenth -= (j - 1)
-                    break
-                if board[processor[n][0] - j][processor[n][1]] == 0:
-                    pass
-                if board[processor[n][0] - j][processor[n][1]] == 1:
-                    break
-        elif i == 1:
-            for j in range(1, N):
-                if processor[n][1] + j == N:
-                    line_lenth += (j - 1)
-                    connect += 1
-                    for k in range(1, j):
-                        board[processor[n][0]][processor[n][1] + k] = 1
-                    sol(n + 1, line_lenth, connect)
-                    for k in range(1, j):
-                        board[processor[n][0]][processor[n][1] + k] = 0
-                    connect -= 1
-                    line_lenth -= (j - 1)
-                    break
-                if board[processor[n][0]][processor[n][1] + j] == 0:
-                    pass
-                if board[processor[n][0]][processor[n][1] + j] == 1:
-                    break
-        elif i == 2:
-            for j in range(1, N):
-                if processor[n][0] + j == N:
-                    line_lenth += (j - 1)
-                    connect += 1
-                    for k in range(1, j):
-                        board[processor[n][0] + k][processor[n][1]] = 1
-                    sol(n + 1, line_lenth, connect)
-                    for k in range(1, j):
-                        board[processor[n][0] + k][processor[n][1]] = 0
-                    connect -= 1
-                    line_lenth -= (j - 1)
-                    break
-                if board[processor[n][0] + j][processor[n][1]] == 0:
-                    pass
-                if board[processor[n][0] + j][processor[n][1]] == 1:
-                    break
-        elif i == 3:
-            for j in range(1, N):
-                if processor[n][1] - j == N:
-                    line_lenth += (j - 1)
-                    connect += 1
-                    for k in range(1, j):
-                        board[processor[n][0]][processor[n][1] - k] = 1
-                    sol(n + 1, line_lenth, connect)
-                    for k in range(1, j):
-                        board[processor[n][0]][processor[n][1] - k] = 0
-                    connect -= 1
-                    line_lenth -= (j - 1)
-                    break
-                if board[processor[n][0]][processor[n][1] - j] == 0:
-                    pass
-                if board[processor[n][0]][processor[n][1] - j] == 1:
-                    break
+    for direction in range(4):
+        if direction == 0:
+            if sum([board[k][processor[n][1]] for k in range(processor[n][0])]) == 0:
+                for l in range(processor[n][0]):
+                    board[l][processor[n][1]] = 1
+                sol(n + 1, connect + 1, line_lenth + processor[n][0])
+                for l in range(processor[n][0]):
+                    board[l][processor[n][1]] = 0
+        elif direction == 1:
+            if sum(board[processor[n][0]][processor[n][1] + 1:]) == 0:
+                for l in range(processor[n][1] + 1, N):
+                    board[processor[n][0]][l] = 1
+                sol(n + 1, connect + 1, line_lenth + N - 1 - processor[n][1])
+                for l in range(processor[n][1] + 1, N):
+                    board[processor[n][0]][l] = 0
+        elif direction == 2:
+            if sum([board[k][processor[n][1]] for k in range(processor[n][0] + 1, N)]) == 0:
+                for l in range(processor[n][0] + 1, N):
+                    board[l][processor[n][1]] = 1
+                sol(n + 1, connect + 1, line_lenth + N - 1 - processor[n][0])
+                for l in range(processor[n][0] + 1, N):
+                    board[l][processor[n][1]] = 0
+        elif direction == 3:
+            if sum(board[processor[n][0]][:processor[n][1]]) == 0:
+                for l in range(processor[n][1]):
+                    board[processor[n][0]][l] = 1
+                sol(n + 1, connect + 1, line_lenth + processor[n][1])
+                for l in range(processor[n][1]):
+                    board[processor[n][0]][l] = 0
+    sol(n + 1, connect, line_lenth)
+
 
 T = int(input())
 for test_case in range(1, T + 1):
@@ -91,9 +53,92 @@ for test_case in range(1, T + 1):
         for j in range(1, N - 1):
             if board[i][j] == 1:
                 processor_cnt += 1
-                processor.append([i, j, 0])
+                processor.append([i, j])
     sol(0, 0, 0)
     print('#{} {}'.format(test_case, ans))
+
+
+
+# 이 방식은 뭔가 잘못 됐다. 디버깅 보다 새로 짜는게 덜 복잡 할 것 같다.
+# def sol(n, line_lenth, connect):
+#     global N, ans, max_connect, processor_cnt
+#     if n == processor_cnt:
+#         if max_connect < connect:
+#             max_connect = connect
+#             ans = line_lenth
+#         elif max_connect == connect and ans > line_lenth:
+#             ans = line_lenth
+#         return
+#     for i in range(4):
+#         if i == 0:
+#             for j in range(1, N):
+#                 if processor[n][0] - j == -1:
+#                     line_lenth += (j - 1)
+#                     connect += 1
+#                     for k in range(1, j):
+#                         board[processor[n][0] - k][processor[n][1]] = 1
+#                     sol(n + 1, line_lenth, connect)
+#                     for k in range(1, j):
+#                         board[processor[n][0] - k][processor[n][1]] = 0
+#                     connect -= 1
+#                     line_lenth -= (j - 1)
+#                     break
+#                 if board[processor[n][0] - j][processor[n][1]] == 0:
+#                     pass
+#                 if board[processor[n][0] - j][processor[n][1]] == 1:
+#                     break
+#         elif i == 1:
+#             for j in range(1, N):
+#                 if processor[n][1] + j == N:
+#                     line_lenth += (j - 1)
+#                     connect += 1
+#                     for k in range(1, j):
+#                         board[processor[n][0]][processor[n][1] + k] = 1
+#                     sol(n + 1, line_lenth, connect)
+#                     for k in range(1, j):
+#                         board[processor[n][0]][processor[n][1] + k] = 0
+#                     connect -= 1
+#                     line_lenth -= (j - 1)
+#                     break
+#                 if board[processor[n][0]][processor[n][1] + j] == 0:
+#                     pass
+#                 if board[processor[n][0]][processor[n][1] + j] == 1:
+#                     break
+#         elif i == 2:
+#             for j in range(1, N):
+#                 if processor[n][0] + j == N:
+#                     line_lenth += (j - 1)
+#                     connect += 1
+#                     for k in range(1, j):
+#                         board[processor[n][0] + k][processor[n][1]] = 1
+#                     sol(n + 1, line_lenth, connect)
+#                     for k in range(1, j):
+#                         board[processor[n][0] + k][processor[n][1]] = 0
+#                     connect -= 1
+#                     line_lenth -= (j - 1)
+#                     break
+#                 if board[processor[n][0] + j][processor[n][1]] == 0:
+#                     pass
+#                 if board[processor[n][0] + j][processor[n][1]] == 1:
+#                     break
+#         elif i == 3:
+#             for j in range(1, N):
+#                 if processor[n][1] - j == N:
+#                     line_lenth += (j - 1)
+#                     connect += 1
+#                     for k in range(1, j):
+#                         board[processor[n][0]][processor[n][1] - k] = 1
+#                     sol(n + 1, line_lenth, connect)
+#                     for k in range(1, j):
+#                         board[processor[n][0]][processor[n][1] - k] = 0
+#                     connect -= 1
+#                     line_lenth -= (j - 1)
+#                     break
+#                 if board[processor[n][0]][processor[n][1] - j] == 0:
+#                     pass
+#                 if board[processor[n][0]][processor[n][1] - j] == 1:
+#                     break
+#     sol(n + 1, line_lenth, connect)
 
 
 
