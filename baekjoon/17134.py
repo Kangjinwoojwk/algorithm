b0469a097546cc11d7eeb2f5b1c4625b59457267
Kangjinwoojwk <kjw03230@naver.com>
2019-04-11 23:43:06 +0900

    algorithm/self/0411

--- baekjoon/17134.py
+++ baekjoon/17134.py
@@ -1,31 +1,21 @@
-import sys
-sys.stdout = open('17134.txt', 'w')
+# import sys
+# sys.stdin = open('17134.txt', 'r')
+# sys.stdout = open('17134_w.txt', 'w')
 from time import time
 a = time()
 ceh = [True] * 1000001
 ceh[0] = ceh[1] = False
 for i in range(2, 1001):
-    if ceh[i] == False:
+    if ceh[i] is False:
         continue
     for j in range(i << 1, 1000000, i):
         ceh[j] = False
 print(time()-a)
 prime = [i for i in range(1000000) if ceh[i]]
 print(time()-a)
-n = len(prime)
-print(n)
 cnt = [0] * 1000001
-for i in range(n):
-    if prime[i] > 5000:
-        break
-    it = prime[i] << 1
-    for j in range(1, n):
-        if it + prime[j] > 10000:
-            break
-        cnt[it + prime[j]] += 1
-print(time()-a)
-for i in range(100):
-    print(cnt[i*100:i*100+100])
 
 # for _ in range(int(input())):
 #     print(cnt[int(input())])
+
+# print(time()-a)
