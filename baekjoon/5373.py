b1beffde411da40c438f705187433661d85037b6
kangjinwoo <kjw03230@naver.com>
2019-08-26 20:04:52 +0900

    algorithm/20190826

--- baekjoon/5373.py
+++ baekjoon/5373.py
@@ -7,7 +7,67 @@ def rotate(a):
     else:
         cube[location][0][0], cube[location][0][2], cube[location][2][2], cube[location][2][0] = cube[location][0][2], cube[location][2][2], cube[location][2][0], cube[location][0][0]
         cube[location][0][1], cube[location][1][2], cube[location][2][1], cube[location][1][0] = cube[location][1][2], cube[location][2][1], cube[location][1][0], cube[location][0][1]
-        
+    if a == 'U+':
+        cube['F'][0][0], cube['F'][0][1], cube['F'][0][2], cube['R'][0][0], cube['R'][0][1], cube['R'][0][2], \
+        cube['B'][0][0], cube['B'][0][1], cube['B'][0][2], cube['L'][0][0], cube['L'][0][1], cube['L'][0][2] = \
+        cube['L'][0][0], cube['L'][0][1], cube['L'][0][2], cube['F'][0][0], cube['F'][0][1], cube['F'][0][2], \
+        cube['R'][0][0], cube['R'][0][1], cube['R'][0][2], cube['B'][0][0], cube['B'][0][1], cube['B'][0][2]
+    elif a == 'U-':
+        cube['L'][0][0], cube['L'][0][1], cube['L'][0][2], cube['F'][0][0], cube['F'][0][1], cube['F'][0][2], \
+        cube['R'][0][0], cube['R'][0][1], cube['R'][0][2], cube['B'][0][0], cube['B'][0][1], cube['B'][0][2] = \
+        cube['F'][0][0], cube['F'][0][1], cube['F'][0][2], cube['R'][0][0], cube['R'][0][1], cube['R'][0][2], \
+        cube['B'][0][0], cube['B'][0][1], cube['B'][0][2], cube['L'][0][0], cube['L'][0][1], cube['L'][0][2]
+    elif a == 'D+':
+        cube['F'][2][0], cube['F'][2][1], cube['F'][2][2], cube['R'][2][0], cube['R'][2][1], cube['R'][2][2], \
+        cube['B'][2][0], cube['B'][2][1], cube['B'][2][2], cube['L'][2][0], cube['L'][2][1], cube['L'][2][2] = \
+        cube['L'][2][0], cube['L'][2][1], cube['L'][2][2], cube['F'][2][0], cube['F'][2][1], cube['F'][2][2], \
+        cube['R'][2][0], cube['R'][2][1], cube['R'][2][2], cube['B'][2][0], cube['B'][2][1], cube['B'][2][2]
+    elif a == 'D-':
+        cube['L'][2][0], cube['L'][2][1], cube['L'][2][2], cube['F'][2][0], cube['F'][2][1], cube['F'][2][2], \
+        cube['R'][2][0], cube['R'][2][1], cube['R'][2][2], cube['B'][2][0], cube['B'][2][1], cube['B'][2][2] = \
+        cube['F'][2][0], cube['F'][2][1], cube['F'][2][2], cube['R'][2][0], cube['R'][2][1], cube['R'][2][2], \
+        cube['B'][2][0], cube['B'][2][1], cube['B'][2][2], cube['L'][2][0], cube['L'][2][1], cube['L'][2][2]
+    elif a == 'L+':
+        cube['F'][0][0], cube['F'][1][0], cube['F'][2][0], cube['D'][0][0], cube['D'][1][0], cube['D'][2][0], \
+        cube['B'][0][2], cube['B'][1][2], cube['B'][2][2], cube['U'][0][0], cube['U'][1][0], cube['U'][2][0] = \
+        cube['U'][0][0], cube['U'][1][0], cube['U'][2][0], cube['F'][0][0], cube['F'][1][0], cube['F'][2][0], \
+        cube['D'][2][0], cube['D'][1][0], cube['D'][0][0], cube['B'][2][2], cube['B'][1][2], cube['B'][0][2]
+    elif a == 'L-':
+        cube['U'][0][0], cube['U'][1][0], cube['U'][2][0], cube['F'][0][0], cube['F'][1][0], cube['F'][2][0], \
+        cube['D'][0][0], cube['D'][1][0], cube['D'][2][0], cube['B'][2][2], cube['B'][2][1], cube['B'][2][0] = \
+        cube['F'][0][0], cube['F'][1][0], cube['F'][2][0], cube['D'][0][0], cube['D'][1][0], cube['D'][2][0], \
+        cube['B'][2][2], cube['B'][1][2], cube['B'][0][2], cube['U'][0][0], cube['U'][0][1], cube['U'][0][2]
+    elif a == 'R+':
+        cube['U'][0][2], cube['U'][1][2], cube['U'][2][2], cube['F'][0][2], cube['F'][1][2], cube['F'][2][2], \
+        cube['D'][0][2], cube['D'][1][2], cube['D'][2][2], cube['B'][0][0], cube['B'][0][1], cube['B'][0][2] = \
+        cube['F'][0][2], cube['F'][1][2], cube['F'][2][2], cube['D'][0][2], cube['D'][1][2], cube['D'][2][2], \
+        cube['B'][2][0], cube['B'][1][0], cube['B'][0][0], cube['U'][2][2], cube['U'][2][1], cube['U'][2][0]
+    elif a == 'R-':
+        cube['F'][0][2], cube['F'][1][2], cube['F'][2][2], cube['D'][0][2], cube['D'][1][2], cube['D'][2][2], \
+        cube['B'][2][0], cube['B'][1][0], cube['B'][0][0], cube['U'][2][2], cube['U'][2][1], cube['U'][2][0] = \
+        cube['U'][0][2], cube['U'][1][2], cube['U'][2][2], cube['F'][0][2], cube['F'][1][2], cube['F'][2][2], \
+        cube['D'][0][2], cube['D'][1][2], cube['D'][2][2], cube['B'][0][0], cube['B'][0][1], cube['B'][0][2]
+    elif a == 'F+':
+        cube['U'][2][0], cube['U'][2][1], cube['U'][2][2], cube['R'][0][0], cube['R'][1][0], cube['R'][2][0], \
+        cube['D'][0][2], cube['D'][0][1], cube['D'][0][0], cube['L'][2][2], cube['L'][1][2], cube['L'][0][2] = \
+        cube['L'][2][2], cube['L'][1][2], cube['L'][0][2], cube['U'][2][0], cube['U'][2][1], cube['U'][2][2], \
+        cube['R'][0][0], cube['R'][1][0], cube['R'][2][0], cube['D'][0][2], cube['D'][0][1], cube['D'][0][0]
+    elif a == 'F-':
+        cube['L'][2][2], cube['L'][1][2], cube['L'][0][2], cube['U'][2][0], cube['U'][2][1], cube['U'][2][2], \
+        cube['R'][0][0], cube['R'][1][0], cube['R'][2][0], cube['D'][0][2], cube['D'][0][1], cube['D'][0][0] = \
+        cube['U'][2][0], cube['U'][2][1], cube['U'][2][2], cube['R'][0][0], cube['R'][1][0], cube['R'][2][0], \
+        cube['D'][0][2], cube['D'][0][1], cube['D'][0][0], cube['L'][2][2], cube['L'][1][2], cube['L'][0][2]
+    elif a == 'B+':
+        cube['L'][2][0], cube['L'][1][0], cube['L'][0][0], cube['U'][0][0], cube['U'][0][1], cube['U'][0][2], \
+        cube['R'][0][2], cube['R'][1][2], cube['R'][2][2], cube['D'][2][2], cube['D'][2][1], cube['D'][2][0] = \
+        cube['U'][0][0], cube['U'][0][1], cube['U'][0][2], cube['R'][0][2], cube['R'][1][2], cube['R'][2][2], \
+        cube['D'][2][2], cube['D'][2][1], cube['D'][2][0], cube['L'][2][0], cube['L'][1][0], cube['L'][0][0]
+    elif a == 'B-':
+        cube['U'][0][0], cube['U'][0][1], cube['U'][0][2], cube['R'][0][2], cube['R'][1][2], cube['R'][2][2], \
+        cube['D'][2][2], cube['D'][2][1], cube['D'][2][0], cube['L'][2][0], cube['L'][1][0], cube['L'][0][0] = \
+        cube['L'][2][0], cube['L'][1][0], cube['L'][0][0], cube['U'][0][0], cube['U'][0][1], cube['U'][0][2], \
+        cube['R'][0][2], cube['R'][1][2], cube['R'][2][2], cube['D'][2][2], cube['D'][2][1], cube['D'][2][0]
+
 
 for tc in range(int(input())):
     cube = {
