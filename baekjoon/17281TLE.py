5d1c83a38e4e243fe1408b1b47bb27870a1d7d5d
kangjinwoo <kjw03230@naver.com>
2019-09-05 12:48:36 +0900

    boj 1713 solve/20190905

--- baekjoon/17281TLE.py
+++ baekjoon/17281TLE.py
@@ -3,51 +3,38 @@ def cal():
     global ans
     score = 0
     j = 0
-    for i in range(N):
+    for i in data:
         out = 0
-        runner = list()
         s = 0
         while out < 3:
-            if data[i][line[j]] == 0:
+            if i[line[j]] == 0:
                 out += 1
             else:
-                runner.append(data[i][line[j]])
+                s = (s << i[line[j]]) + (1 << i[line[j]])
             j = (j + 1) % 9
-        for k in [-1, -2, -3, -4]:
-            s += runner[k]
-            if s >= 4:
-                if k == - 1:
-                    score += len(runner)
-                else:
-                    score += len(runner[:k + 1])
-                break
+        score += bin(s >> 4).count('1')
     if score > ans:
         ans = score
 
-
 def lin(n):
-    if n == 9:
+    if n == 0:
         cal()
         return
-    if n == 3:
-        lin(4)
+    if n == 4:
+        lin(n - 1)
     else:
-        for i in [1, 2, 3, 4, 5, 6, 7, 8]:
-            if visited[i]:
-                visited[i] = False
-                line[n] = i
-                lin(n + 1)
-                visited[i] = True
-
+        for k in range(n):
+            if k == 3:
+                continue
+            line[k], line[n - 1] = line[n - 1], line[k]
+            lin(n - 1)
+            line[n - 1], line[k] = line[k], line[n - 1]
 
 N = int(input())
 data = [list(map(int, input().split())) for _ in range(N)]
 t = time.time()
-visited = [True] * 9
-line = [0] * 9
+line = [1, 2, 3, 0, 4, 5, 6, 7, 8]
 ans = 0
-visited[0] = False
-line[3] = 0
-lin(0)
+lin(9)
 print(time.time() - t)
 print(ans)
--- test.py
+++ test.py
@@ -1,21 +1,22 @@
 import math
 import time
-a = list()
-b = list()
-c = list()
+import sys
+sys.setrecursionlimit(200000)
+def sol(n):
+    if n == 10:
+        return
+    for i in range(10):
+        if visit[i]:
+            visit[i] = False
+            a[n] = i
+            sol(n + 1)
+            visit[i] = True
+a = [0]*10
+visit = [True] * 10
 t = time.time()
-for i in range(1000000):
-    a.append(i)
+sol(0)
 at = time.time()
 print(at - t)
-a.reverse()
-at_ = time.time()
-print(at_ - at)
-for i in range(100000):
-    b.insert(0, i)
+
 bt = time.time()
 print(bt - at)
-for i in range(100000):
-    c = [0] + c
-ct = time.time()
-print(ct - bt)
