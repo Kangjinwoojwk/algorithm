6738e0c6439f1c990b69e5c38868aa4de3a31e14
Kangjinwoojwk <kjw03230@naver.com>
2020-08-29 22:47:50 +0900

    programmers solve again/20200829

--- programmers/주식가격.py
+++ programmers/주식가격.py
@@ -1,3 +1,17 @@
+# 정답 코드, 한번만 보려고 발악했는데 그럴 필요 없다 n^2가 낫다
+def solution(prices):
+    n = len(prices)
+    answer = [0] * n
+    for i in range(n):
+        for j in range(i + 1, n):
+            if prices[i] > prices[j]:
+                answer[i] = j - i
+                break
+        else:
+            answer[i] = j - i
+
+    return answer
+
 
 
 # 정확성 통과 효율성 전부 시간초과
