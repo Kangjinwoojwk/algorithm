eaf0b6b8244e05afb793960e86e89f271316112a
KangJinwoo <kjw03230@naver.com>
2021-09-22 22:00:02 +0900

    restAPI practice/210922

--- programmers/RestAPI_pratice.py
+++ programmers/RestAPI_pratice.py
@@ -1,10 +1,18 @@
 import requests
+import urllib.request
 import json
 
-client_id = "kc2pT9AIwE9nQiNbkGCr"
-client_secret = "3_1niwGG_X"
-url = "https://openapi.naver.com/v1/datalab/search"
-body = json
-print(body)
+url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=901"
 request = requests.get(url)
+data = request.content
+da = json.loads(data)
 print(request)
+print(data)
+print(da)
+
+request = urllib.request.Request(url)
+response_body = urllib.request.urlopen(request).read()
+response = json.loads(response_body)
+print(request)
+print(response_body)
+print(response)
